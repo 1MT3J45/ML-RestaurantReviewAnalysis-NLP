4c9d329d82cb73d6bac735a3f6ee4ae8eea853a2
1mt3j45 <agenttj.rawal98@gmail.com>
2018-05-07 21:24:30 +0530

    Bag of Words with Spell Corrections generated

--- MainWave.py
+++ MainWave.py
@@ -10,8 +10,12 @@ import numpy as np
 
 import re
 import spacy
+from nltk.corpus import wordnet
+from autocorrect import spell
+
 import progressbar as bar
 import extractUnique as xq
+import pickle
 
 testB = pd.read_csv("CSV/Restaurants_Test_Data_phaseB.csv")
 trainB = pd.read_csv("CSV/Restaurants_Train_v2.csv")
@@ -24,10 +28,12 @@ testB_1 = testB.iloc[:, [0, 5, 4]]
 
 fullB = pd.concat([trainB_1], axis=0, ignore_index=True)
 
-#nltk.download('stopwords')
+# nltk.download('stopwords')
 
 
-dataset = fullB     # MAJOR DATA-SET
+dataset = fullB  # MAJOR DATA-SET
+
+
 # --------------------- FUNCTIONS --------------------------
 
 
@@ -51,14 +57,14 @@ def check_dep_parse(token_dep):
 
 # --------------------- STREAM INITIALIZER ----------------------------
 PoS_Tag_sent = list()
-S1_corpus = []          # CORPUS (For Collecting Lemmas)
-corpora = ''            # CORPORA (For Collecting Corpora of single sentence)
-S2_super_corpus =[]     # CORPUS (For Collecting Bigrams sentence wise)
+S1_corpus = []  # CORPUS (For Collecting Lemmas)
+corpora = ''  # CORPORA (For Collecting Corpora of single sentence)
+S2_super_corpus = []  # CORPUS (For Collecting Bigrams sentence wise)
 
 # --------------------- SPACY SPECS ------------------------
 nlp_en = spacy.load('en_core_web_sm')
-plot_nlp = 0            # For Plotting of Dependency chart
-S3_dep_corpus = []      # CORPUS (For Collecting Dependency Relations)
+plot_nlp = 0  # For Plotting of Dependency chart
+S3_dep_corpus = []  # CORPUS (For Collecting Dependency Relations)
 
 # ---------------------------------------------------------- STREAM 1 - LEMMATIZATION
 try:
@@ -71,7 +77,8 @@ try:
         ps = PorterStemmer()
         nl = WordNetLemmatizer()
 
-        review = [ps.stem(nl.lemmatize(word, pos='v')) for word in review if not word in set(stopwords.words('english'))]
+        review = [ps.stem(nl.lemmatize(word, pos='v')) for word in review if
+                  not word in set(stopwords.words('english'))]
         review = list(set(review))
         S1_corpus.append(review)
         bar.load(i, base=dataset, text='Stream 1')
@@ -84,58 +91,61 @@ try:
         PoS_Tag_sent = nltk.pos_tag(sent)
 
         for (w1, tag1), (w2, tag2) in nltk.bigrams(PoS_Tag_sent):
-            if tag1.startswith('JJ') and tag2.startswith('NN'):     # R1
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('RB') and tag2.startswith('JJ'):   # R2
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('JJ') and tag2.startswith('JJ'):   # R3
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('NN') and tag2.startswith('JJ'):   # R4
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('RB') and tag2.startswith('VB'):   # R5
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('VB') and tag2.startswith('NN'):   # R6
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('JJ') and tag2.startswith('VB'):   # R7
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('RB') and tag2.startswith('RB'):   # R8
-                corpora += w1+' '+w2+';'
-            elif tag1.startswith('RB') and tag2.startswith('VB'):   # R9
-                corpora += w1+' '+w2+';'
+            if tag1.startswith('JJ') and tag2.startswith('NN'):  # R1
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('RB') and tag2.startswith('JJ'):  # R2
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('JJ') and tag2.startswith('JJ'):  # R3
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('NN') and tag2.startswith('JJ'):  # R4
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('RB') and tag2.startswith('VB'):  # R5
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('VB') and tag2.startswith('NN'):  # R6
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('JJ') and tag2.startswith('VB'):  # R7
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('RB') and tag2.startswith('RB'):  # R8
+                corpora += w1 + ' ' + w2 + ';'
+            elif tag1.startswith('RB') and tag2.startswith('VB'):  # R9
+                corpora += w1 + ' ' + w2 + ';'
 
         S2_super_corpus.append(corpora)
         corpora = ''
         bar.load(i, base=dataset, text='Stream 2')
     print('Stream 2: Processed')
 except KeyboardInterrupt:
-        print("Terminating. Human Intervention Not Allowed")
-        exit(0)
+    print("[STAGE 1] Terminating. Human Intervention Not Allowed")
+    exit(0)
+except AttributeError as e:
+    print("[STAGE 1] Terminating. Human Intervention Not Allowed")
+    exit(0)
 # ----------------------------------------------------------- STREAM 3 - DEPENDENCY FEATURES (spaCy)
 
 try:
     for increment in range(len(dataset)):
         sentence = dataset.iloc[increment, 0].lower()
-        #print(increment)
+        # print(increment)
         for token in nlp_en(sentence):
             dep = check_dep_parse(token.dep_)
             if dep is True:
                 # print(token.dep_, end="> ")
                 # print(token.head, token)
-                corpora += str(token)+' '+str(token.head)+';'
+                corpora += str(token) + ' ' + str(token.head) + ';'
             else:
                 pass
         S3_dep_corpus.append(corpora)
         corpora = ''
-        bar.load(increment,base=dataset, text='Stream 3')
+        bar.load(increment, base=dataset, text='Stream 3')
     print('Stream 3: Processed')
     plot_nlp = nlp_en(sentence)
 
     pass
 except TypeError as e:
-    print("Unexpected Termination:", e)
+    print("[STAGE 2] Unexpected Termination:", e)
     exit(0)
 except KeyboardInterrupt:
-    print("Human Interrupt Received! Exiting...")
+    print("[STAGE 2] Human Interrupt Received! Exiting...")
     exit(0)
 
 stream1 = pd.Series(S1_corpus)
@@ -144,7 +154,7 @@ stream3 = pd.Series(S3_dep_corpus)
 df = pd.concat([stream1, stream2, stream3], axis=1)
 df = df.rename(columns={0: 'lemmas', 1: 'bigrams', 2: 'depenrel'})
 df.to_csv('FeatureSet.csv', index=False)
-df =pd.read_csv('FeatureSet.csv', sep=',')
+df = pd.read_csv('FeatureSet.csv', sep=',')
 # try:
 #     pool = ThreadPool(2)
 #     pool.map(os.system('firefox localhost:5000 &'), spacy.displacy.serve(plot_nlp, style='dep')).join()
@@ -155,55 +165,98 @@ df =pd.read_csv('FeatureSet.csv', sep=',')
 #     print("Browser must start with Graph. If doesn't please make sure to use Ubuntu with Firefox")
 
 # Get Unique Features from Bigrams, Depen Rel
-whole_df = pd.concat([dataset.iloc[0:,0],stream1, stream2, stream3, dataset.iloc[0:,2]], axis=1)
-whole_df = whole_df.rename(columns={'text': 'reviews', 0: 'lemmas', 1: 'bigrams', 2: 'depenrel', 'aspectCategories/aspectCategory/0/_category': 'aspectCategory'})
+whole_df = pd.concat([dataset.iloc[0:, 0], stream1, stream2, stream3, dataset.iloc[0:, 2]], axis=1)
+whole_df = whole_df.rename(columns={'text': 'reviews', 0: 'lemmas', 1: 'bigrams', 2: 'depenrel',
+                                    'aspectCategories/aspectCategory/0/_category': 'aspectCategory'})
 whole_df.to_csv('WholeSet.csv', index=False)
 whole_df = pd.read_csv('WholeSet.csv', sep=',')
 try:
-    u_feat = xq.unique(whole_df=whole_df,bigram_col=2,dep_rel_col=3)
+    u_feat = xq.unique(whole_df=whole_df, bigram_col=2, dep_rel_col=3)
     print("Unique Features Extracted")
 except KeyboardInterrupt:
-    print("Manual Interrupt to Unique Features")
+    print("[STAGE 3] Manual Interrupt to Unique Features")
     exit(0)
 except Exception as e:
-    print('Improper Termination due to:',e)
+    print('[STAGE 3] Improper Termination due to:', e)
     exit(0)
 # DF with Review, Lemmas, U_feat, Aspect Cat
-Feature_df = whole_df[['reviews','lemmas']][0:]
-Feature_df = pd.concat([Feature_df,pd.Series(u_feat),whole_df.iloc[0:,-1]], axis=1)
+Feature_df = whole_df[['reviews', 'lemmas']][0:]
+Feature_df = pd.concat([Feature_df, pd.Series(u_feat), whole_df.iloc[0:, -1]], axis=1)
 Feature_df = Feature_df.rename(columns={0: 'ufeat'})
 Feature_df.to_csv('Feature.csv', index=False)
 
 # Aspect Cat, Lemmas + U_feat (from All sentences)
+c_list = list()
 try:
     c_list = xq.combiner(Feature_df=Feature_df, lemma_col=1, uniqueFeat_col=2)
 except KeyboardInterrupt:
-    print("Manual Interrupt to Combiner")
+    print("[STAGE 4] Manual Interrupt to Combiner")
     exit(0)
 except Exception as e:
-    print('Improper Termination due to:',e)
+    print("[STAGE 4] Improper Termination due to:", e)
     exit(0)
-n = np.array(c_list)
-n = pd.Series(n)
-Process_df = pd.concat([Feature_df['reviews'][0:], n, Feature_df['aspectCategory'][0:]], axis=1).\
-    rename(columns={0: 'combinedFeatures'})
-Process_df = Process_df.rename(columns={0: 'combinedFeatures'})
-Process_df.to_csv('Process.csv', index=False)
-# TODO --- DF > Aspect Cat, Lemmas + U_feat = X , Synon.ConceptNet(X), Synon.SentiWordNet(X)
-from nltk.corpus import wordnet
+ngram_list = list()
+try:
+    ngram_list = xq.get_correct_spell(word_list=c_list)
+    pickle.dumps(ngram_list)
+except ValueError:
+    print("[STAGE 5] Spell Checker | Interrupted")
+except AttributeError:
+    print("[STAGE 5] Spell Checker | Attrition")
+except KeyboardInterrupt:
+    print("[STAGE 5] Spell Checker | Forced Drop")
 
-syns = wordnet.synsets('program')
+from sklearn.feature_extraction.text import CountVectorizer
+# Creating Bag of Words Model
 
-from nltk.corpus import wordnet
-from autocorrect import spell
-synonyms = []
-antonyms = []
-correct_word = spell('thanos')
-for syn in wordnet.synsets(correct_word):
-    for l in syn.lemmas():
-        synonyms.append(l.name())
-        if l.antonyms():
-            antonyms.append(l.antonyms()[0].name())
-print(set(synonyms))
-print(set(antonyms))
-# TODO --- ML over DF
+cv = CountVectorizer(max_features=33433, ngram_range=(1, 2))
+cv.fit_transform(ngram_list)
+itemDict = cv.vocabulary_
+print(cv.vocabulary_)
+
+key_Book = pd.DataFrame(itemDict, index=range(itemDict.__len__()))
+key_Book.to_csv('key_Book.csv', index=True, sep=',')
+
+# n = np.array(c_list)
+# n = pd.Series(n)
+# Process_df = pd.concat([Feature_df['reviews'][0:], n, Feature_df['aspectCategory'][0:]], axis=1). \
+#     rename(columns={0: 'combinedFeatures'})
+# Process_df = Process_df.rename(columns={0: 'combinedFeatures'})
+# Process_df.to_csv('Process.csv', index=False)
+# # TODO --- Pivot DF >  AspectCat, Combined_feat
+# Pivot_df = Process_df.iloc[0:, [2, 1]]
+# Pivot_df = Pivot_df.groupby('aspectCategory')['combinedFeatures'].apply(list).reset_index()
+# Pivot_df.to_csv('Pivot.csv')
+#
+# # TODO --- DF > Aspect Cat, Lemmas + U_feat = X , Synon.ConceptNet(X), Synon.SentiWordNet(X)
+# from nltk.corpus import sentiwordnet as swn
+# from nltk.corpus.reader.lin import LinThesaurusCorpusReader as ltcr
+#
+# synon = ltcr.synonyms(ngram='so horrible')
+# print(synon)
+#
+# sentisynset = swn.senti_synsets('horrible')
+# print(list(sentisynset))
+# synonyms = []
+# correct_word = spell('horribl')
+# for syn in wordnet.synsets(correct_word):
+#     for l in syn.lemmas():
+#         synonyms.append(l.name())
+#         # if l.antonyms():      # This can be used to find antonyms
+#         #     antonyms.append(l.antonyms()[0].name())
+#         # else:
+#         #     pass
+# if len(synonyms) is 0:
+#     synonyms.append('blank')
+#
+# print(set(synonyms))
+# # --------------------------- GET SYNONYMS --------------------------------
+# for i in range(len(Pivot_df)):
+#     features = Pivot_df.iloc[i, 1]
+#     print('I = ',i)
+#     for j in range(len(features)):
+#         print(features[j].split(';'), '\n J = ', j)
+#         wordlist = features[j].split(';')
+#         syns = xq.synset_ngram(ngram=wordlist)
+#         print(syns)
+# # TODO --- ML over DF
--- extractUnique.py
+++ extractUnique.py
@@ -38,8 +38,70 @@ def combiner(Feature_df, lemma_col, uniqueFeat_col):
     dataframe = Feature_df
     c_list = list()
     for i in range(len(dataframe)):
-        sentence = ast.literal_eval(dataframe.iloc[i,lemma_col]) + dataframe.iloc[i, uniqueFeat_col]
-        sentence = ' '.join(sentence)
+        sentence = ast.literal_eval(dataframe.iloc[i, lemma_col]) + dataframe.iloc[i, uniqueFeat_col]
+        # fetching list from string
+        sentence = ';'.join(sentence)
         c_list.append(sentence)
-        pb.load(i,dataframe,'Combining Lemma & Ufeat')
+        pb.load(i, dataframe, 'Combining Lemma & Ufeat')
     return c_list
+
+
+def synset_ngram(ngram):
+    syn02 = syn10 = ''
+    for i in range(len(ngram)):
+        incoming = ngram[i].split(sep=' ')
+        print("Iteration ", i)
+        if len(incoming) > 1:
+            # print('Bigram', incoming[0], incoming[1])
+            syn00 = get_correct_syns(word=incoming[0])
+            syn01 = get_correct_syns(word=incoming[1])
+            syn02 = syn00[0]+' '+syn01[0]
+            # print(syn02)
+        elif len(incoming) == 1:
+            # print('Unigram', incoming[0])
+            syn10 = get_correct_syns(incoming[0])
+    op = [syn02, syn10]
+    return op
+
+
+def get_correct_syns(word):
+    from nltk.corpus import wordnet
+    from autocorrect import spell
+
+    synonyms = []
+    correct_word = spell(word)
+    for syn in wordnet.synsets(correct_word):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+
+    if len(synonyms) is 0:
+        synonyms.append('blank')
+
+    brk_sent = list(set(synonyms))
+    brk_sent = nltk.pos_tag(brk_sent)
+    #print(brk_sent)
+    synonyms = []
+    for w, tag in brk_sent:
+        #print(w, " - ", tag)
+        if tag.startswith("NN") or tag.startswith("JJ"):
+            synonyms.append(w)
+        else:
+            pass# print(w, "is not an NOUN")
+    #print(synonyms)
+
+    return synonyms
+
+
+def get_correct_spell(word_list):
+    from autocorrect import spell
+    li = word_list
+    spell_right = list()
+    for i in range(len(li)):
+        w_li = word_list[i].split(';')
+        print(w_li)
+        for word in w_li:
+            cw = spell(word)
+            spell_right.append(cw.lower())
+        print(spell_right)
+        pb.load(i, li, 'Correcting Words')
+    return spell_right
